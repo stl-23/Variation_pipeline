9196aff72db5823d5781d5acd0d3c2e8b76af541
stl-23 <73641317+stl-23@users.noreply.github.com>
2021-03-29 18:18:02 +0800

    Add files via upload

--- run_variation.py
+++ run_variation.py
@@ -6,31 +6,57 @@ import mapping
 import annotation
 import ngs_vars
 import tgs_vars
-from vartools import parsering, getmyconfig
+from vartools import parsering
 
-script_path = os.path.abspath("./")
-make_index = os.path.join(script_path, '/tools/index.sh')
+#script_path = os.path.abspath("./")
+#make_index = os.path.join(script_path, '/tools/index.sh')
 
-def main():
-    input_path = os.path.abspath(self.inputs) + '/'
-    out_path = os.path.abspath(self.outputs) + '/'
-    # lst = os.listdir(input_path)
-    outfile = []
-    samples, input1, input2, output1, output2 = parsering.parse_short_read_dir(self.input_path, self.out_path,
-                                                                               self.type)
+def rw_mapping():
+    try:
+        input_path = os.path.abspath(inputs_dir) + '/'
+        out_path = os.path.abspath(outputs_dir) + '/mapping_results'
+        os.system('mkdir -p {out}'.format(out=out_path))
+        # lst = os.listdir(input_path)
+        outfile = []
+        if platform == "ngs":
+            samples, input1, input2, output1, output2 = parsering.parse_short_read_dir(input_path, out_path, seqtype)
 
-    if platform == "NGS":
-        out1,out2,out3 = ngs_vars.ngs_snp_indel(ref, sample, tool, combine,BQSR,gcvf,known_site)
-        if combine == "N" and tool == 'samtools':
-            fw = open('samtools_snp_indel.sh','w').write(out1)
-        elif combine == 'N' and tool == 'gatk':
-            fw = open('gatk_snp_indel.sh','w').write(out2)
-        elif combine == 'Y' and tool == 'samtools+gatk':
-            fw = open('samtools_snp_indel.sh','w').write(out1)
-            fw = open('gatk_snp_indel.sh','w').write(out2)
-            fw = open('samtools_gatk_combine.sh','w').write(out3)
+            outfile = mapping.Ngs(maptool, input_path, out_path,ref,maptool_parameters,seqtype).ngs()
+            for i,s in enumerate(outfile):
+                fw = open('ngs'+i+'.bwa.sh','w').write(s)
 
-if __name__ == "main":
+        elif platform == 'tgs':
+            samples, input1 = parsering.parse_long_read_dir(input_path)
+            if maptool == 'minimap2':
+                outfile = mapping.Tgs(maptool,input_path,out_path,ref,maptool_parameters).tgs_minimap2()
+                for i,s in enumerate(outfile):
+                    fw = open('tgs'+i+'.minimap2.sh','w').write(s)
+            elif maptool == 'ngml':
+                outfile = mapping.Tgs(maptool,input_path,out_path,ref,maptool_parameters).tgs_ngml()
+                for i,s in enumerate(outfile):
+                    fw = open('tgs'+i+'.ngml.sh','w').write(s)
+    except Exception as e:
+        print('Error files or mapping tools')
+
+def rw_call_var():
+    try:
+        input_path = os.path.abspath(outputs_dir) + '/'
+        out_path = os.path.abspath(outputs_dir) + '/var_results'
+        os.system('mkdir -p {out}'.format(out=out_path))
+        # lst = os.listdir(input_path)
+        outfile = []
+        if platform == 'ngs':
+            if mode == ''
+        elif platform == 'tgs':
+
+def rw_annotation():
+    try:
+        input_path = os.path.abspath(outputs_dir) + '/var_results'
+        out_path = os.path.abspath(outputs_dir) + '/annotation_results'
+        os.system('mkdir -p {out}'.format(out=out_path))
+
+
+if __name__ == '__main__':
     examplelog = """EXAMPLES:
     python run_variation.py -i /root/my_data/cleandata/ -o /root/my_data/results/ -r /root/my_data/ref/hg19.fa -g  
     /root/my_data/ref/hg19.gff3 -sp ngs -mt BWA -ct samtools -mode SNP+Indel
@@ -40,8 +66,9 @@ if __name__ == "main":
     python run_variation.py -i /root/my_data/cleandata/ -o /root/my_data/results/ -r /root/my_data/ref/hg19.fa -g 
     /root/my_data/ref/hg19.gff3 -sp ngs -mt BWA -ct gatk4 -pr 'Y' -s1 Tu4 -s2 Nm_35 -mode SNP+Indel
     python run_variation.py -i /root/my_data/cleandata/ -o /root/my_data/results/ -r /root/my_data/ref/hg19.fa -g 
-    /root/my_data/ref/hg19.gff3 -sp ngs -mt BWA -ct control-freec -pr 'N' -mode CNV
-    
+    /root/my_data/ref/hg19.gff3 -sp ngs -mt BWA -ct control-freec -pr 'N' -mode CNV -ot WGS 
+    python run_variation.py -i /root/my_data/cleandata/ -o /root/my_data/results/ -r /root/my_data/ref/hg19.fa -g 
+    /root/my_data/ref/hg19.gff3 -sp tgs -mt minimap2 -ct sniffles -mode SV 
     """
     parser = argparse.ArgumentParser(description='Variation pipline v1.0',
                                      epilog=examplelog,
@@ -59,7 +86,9 @@ if __name__ == "main":
                          help="The gff3 file of reference for variation sites annotation")
     mapref = parser.add_argument_group(title='Mapping options')
     mapref.add_argument('-sp', '--seq_platform',type=str, default='ngs',choices=['ngs','tgs'],
-                        help='Reads sequencing types, ngs for illumina short reads; tgs for Pacbio or ONT long reads')
+                        help='Reads sequencing platform, ngs for illumina short reads; tgs for Pacbio or ONT long reads')
+    mapref.add_argument('-st', '--seq_type', type=str, default='PE', choices=['PE', 'SE'],
+                        help='Sequencing type, paired-end/mate-pair, or single end')
     mapref.add_argument('-mt', '--maptool', type=str, default='BWA', choices=['BWA','Minimap2','NGML'],
                         help='Choose an alignment tool,illumina:BWA;Pacbio/ONT:Minimap2/NGML')
     mapref.add_argument('-mp', '--maptool_parameters',type=str,default='',
@@ -85,7 +114,29 @@ if __name__ == "main":
                           help='The target(tumor/disease) sample name')
     mutation.add_argument('-s2', '--control', type=str,
                            help='The control(normal) sample name')
+    mutation.add_argument('-sg', '--strategy', type=str, default='WGS',choices=['WGS','WES'],
+                          help='WGS or WES')
+    args = parser.parse_args()
+    inputs_dir = args.input
+    outputs_dir = args.outputs
+    ref = args.reference
+    gff3 = args.gff3
+    platform = args.seq_platform
+    seqtype = args.seq_type
+    maptool = args.maptool
+    maptool_parameters = args.maptool_parameters
+    calltool = args.calltool
+    mode = args.mode
+    bqsr = args.BQSR
+    vqsr = args.VQSR
+    pairs = args.pairs
+    sample = args.sample
+    control = args.control
+    strategy = args.strategy
 
+    rw_mapping()
+    rw_call_var()
+    rw_annotation()
 
 
 
