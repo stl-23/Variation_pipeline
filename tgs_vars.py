be3ad9c627630762c9b93b6535df1f216187c55c
stl-23 <73641317+stl-23@users.noreply.github.com>
2021-04-06 17:15:55 +0800

    Add files via upload

--- tgs_vars.py
+++ tgs_vars.py
@@ -1,11 +1,25 @@
 
 from vartools import getmyconfig
 
+gatk4 = getmyconfig.getConfig('Variation', 'gatk4')
 samtools = getmyconfig.getConfig('Variation', 'samtools')
 sniffles = getmyconfig.getConfig('Variation', 'Sniffles')
 
-def tgs_snp_indel():
-    pass
+def tgs_snp_indel(ref,input,sample):
+    ### gatk4 pipeline ###
+    outfile = """{gatk4} HaplotypeCaller -R {ref} -I {input}.sorted.bam \\
+ --pcr_indel_model AGGRESSIVE \\
+ --annotation-group AS_StandardAnnotation \\
+ --minimum-mapping-quality 60 \\
+-O {sample}.vcf
+
+{gatk4} SelectVariants  -R {ref} -V {sample}.vcf --select-type-to-include SNP -O {sample}.raw.snp.vcf
+{gatk4} SelectVariants  -R {ref} -V {sample}.vcf --select-type-to-include INDEL -O {sample}.raw.indel.vcf
+
+{gatk4} VariantFiltration -R {ref} -V {sample}.raw.snp.vcf --filter-expression \\
+
+""".format(gatk4=gatk4, ref=ref, input=input,sample=sample)
+    return outfile
 
 def tgs_sv(sample,tool='sniffles',sniffle_p='-s 1 -d 600 --genotype --cluster --ccs_reads'):
     outfile = ''
