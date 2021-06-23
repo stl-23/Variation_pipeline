7400e702898d227ef3cd2d2df00b68ad123c56f5
stl <73641317+stl-23@users.noreply.github.com>
2021-06-23 10:26:32 +0800

    Add files via upload

--- vartools/somatic_detection.py
+++ vartools/somatic_detection.py
@@ -1,27 +1,37 @@
 import vartools.getmyconfig as getmyconfig
 gatk4 = getmyconfig.getConfig('Variation', 'gatk4')
+bcftools = getmyconfig.getConfig('Variation', 'bcftools')
 
-def mutect2(input_dir,output_dir,ref,tumor,normal,interval,pon=None,germline=None,af=0.001,mem=3,normals_pon=None):
+def mutect2(input_dir,output_dir,tmp_dir,ref,tumor,normal,interval,pon=None,germline=None,af=0.001,mem=3,threads=3,
+            normals_pon=None):
     cmd_somatic = ''
     cmd_create_pon = []
     if not pon and normals_pon:
         normals_pon = [sample for sample in normals_pon.strip().split(',')]
         pon_vcfs = []
         for sample in normals_pon:
-            cmd_create_pon.append("""{gatk4} --java-options "-Xmx{mem}g" Mutect2 \\
--R {ref} -L {interval} \\
--I {input}/{sample}.rmdup.bam -tumor {sample} \\
---disable-read-filter MateOnSameContigOrNoMappedMateReadFilter \\
--O {out}/{sample}.make.pon.vcf.gz""".format(gatk4=gatk4,mem=mem,ref=ref,sample=sample,interval=interval,
-            input=input_dir,out=output_dir))
-            pon_vcfs.append(output_dir+'/'+sample+'.make.pon.vcf.gz')
+            cmd_create_pon.append("""##  Run Mutect2 in tumor-only mode for each normal sample
+{gatk4} --java-options "-Xmx{mem}g" Mutect2 \\
+-R {ref} \\
+-I {input}/{sample}.rmdup.bam \\
+-max-mnp-distance 0 \\
+-O {tmp_dir}/{sample}.make.pon.vcf.gz""".format(gatk4=gatk4,mem=mem,ref=ref,sample=sample,interval=interval,
+            input=input_dir,tmp_dir=tmp_dir))
+            pon_vcfs.append(tmp_dir+'/'+sample+'.make.pon.vcf.gz')
             pon_vcfs_new = ' '.join(['-V '+ i for i in pon_vcfs])
         if germline:
             cmd_somatic = """### create PON ####
-{gatk4} --java-options "-Xmx{mem}g" CreateSomaticPanelOfNormals \\
+{gatk4} --java-options "-Xmx{mem}g" GenomicsDBImport \\
 -R {ref} -L {interval} \\
 {pon_vcfs_new} \\
--O {out}/all_sample.prepon.vcf.gz 
+--reader-threads {threads} \\
+--genomicsdb-workspace-path {tmp_dir}/pon_db
+
+{gatk4} --java-options "-Xmx{mem}g" CreateSomaticPanelOfNormals \\
+-R {ref}  \\
+--germline-resource {germline} \\
+-V gendb://{tmp_dir}/pon_db \\
+-O {tmp_dir}/all_sample.pon.vcf.gz 
 
 ###somatic muation###
 {gatk4} --java-options "-Xmx{mem}g" Mutect2 \\
@@ -30,7 +40,7 @@ def mutect2(input_dir,output_dir,ref,tumor,normal,interval,pon=None,germline=Non
 -I {input}/{normal}.rmdup.bam \\
 -tumor {tumor} \\
 -normal {normal} \\
--pon {out}/all_sample.prepon.vcf.gz \\
+-pon {tmp_dir}/all_sample.pon.vcf.gz \\
 --germline-resource {germline} \\
 --af-of-alleles-not-in-resource {af} \\
 --disable-read-filter MateOnSameContigOrNoMappedMateReadFilter \\
@@ -60,17 +70,28 @@ def mutect2(input_dir,output_dir,ref,tumor,normal,interval,pon=None,germline=Non
 
 ### filter
 {gatk4} --java-options "-Xmx{mem}g" FilterMutectCalls \\
+-R {ref} \\
 -V {out}/{tumor}_{normal}_somatic_m2.vcf.gz \\
 --contamination-table {out}/{tumor}.calculatecontamination.table \\
 -O {out}/{tumor}_{normal}_somatic_m2.filter.vcf.gz
+
+zcat {out}/{tumor}_{normal}_somatic_m2.filter.vcf.gz |egrep '#|PASS' | {bcftools} view -Oz -o {out}/{tumor}_{normal}_somatic_m2.filter.pass.vcf.gz
+{gatk4} IndexFeatureFile -I  {out}/{tumor}_{normal}_somatic_m2.filter.pass.vcf.gz
 """.format(gatk4=gatk4,mem=mem,ref=ref,interval=interval,pon_vcfs_new=pon_vcfs_new,out=output_dir,
-           input=input_dir,tumor=tumor,normal=normal,germline=germline,af=af)
+           input=input_dir,tumor=tumor,normal=normal,germline=germline,af=af,bcftools=bcftools,
+           tmp_dir=tmp_dir,threads=threads)
         else:
             cmd_somatic = """### create PON ###
-{gatk4} --java-options "-Xmx{mem}g" CreateSomaticPanelOfNormals \\
+{gatk4} --java-options "-Xmx{mem}g" GenomicsDBImport \\
 -R {ref} -L {interval} \\
 {pon_vcfs_new} \\
--O {out}/all_sample.prepon.vcf.gz \\
+--reader-threads {threads} \\
+--genomicsdb-workspace-path {tmp_dir}/pon_db
+
+{gatk4} --java-options "-Xmx{mem}g" CreateSomaticPanelOfNormals \\
+-R {ref}  \\
+-V gendb://{tmp_dir}/pon_db \\
+-O {tmp_dir}/all_sample.pon.vcf.gz 
 
 ### somatic mutation ###
 {gatk4} --java-options "-Xmx{mem}g"  Mutect2 \\
@@ -79,16 +100,19 @@ def mutect2(input_dir,output_dir,ref,tumor,normal,interval,pon=None,germline=Non
 -I {input}/{normal}.rmdup.bam \\
 -tumor {tumor} \\
 -normal {normal} \\
--pon {out}/all_sample.prepon.vcf.gz \\
+-pon {tmp_dir}/all_sample.pon.vcf.gz \\
 --disable-read-filter MateOnSameContigOrNoMappedMateReadFilter \\
 -O {out}/{tumor}_{normal}_somatic_m2.vcf.gz
 
 ### filter ###
 {gatk4} --java-options "-Xmx{mem}g" FilterMutectCalls \\
+-R {ref} \\
 -V {out}/{tumor}_{normal}_somatic_m2.vcf.gz \\
 -O {out}/{tumor}_{normal}_somatic_m2.filter.vcf.gz
+zcat {out}/{tumor}_{normal}_somatic_m2.filter.vcf.gz |egrep '#|PASS' | {bcftools} view -Oz -o {out}/{tumor}_{normal}_somatic_m2.filter.pass.vcf.gz
+{gatk4} IndexFeatureFile -I  {out}/{tumor}_{normal}_somatic_m2.filter.pass.vcf.gz
 """.format(gatk4=gatk4, mem=mem, ref=ref, interval=interval, pon_vcfs_new=pon_vcfs_new, out=output_dir,
-                       input=input_dir, tumor=tumor, normal=normal)
+           input=input_dir, tumor=tumor, normal=normal,bcftools=bcftools,tmp_dir=tmp_dir,threads=threads)
     elif pon and not normals_pon:
         if germline:
             cmd_somatic = """### somatic mutation ###
@@ -128,11 +152,14 @@ def mutect2(input_dir,output_dir,ref,tumor,normal,interval,pon=None,germline=Non
 
 ### filter
 {gatk4} --java-options "-Xmx{mem}g" FilterMutectCalls \\
+-R {ref} \\
 -V {out}/{tumor}_{normal}_somatic_m2.vcf.gz \\
 --contamination-table {out}/{tumor}.calculatecontamination.table \\
 -O {out}/{tumor}_{normal}_somatic_m2.filter.vcf.gz
+zcat {out}/{tumor}_{normal}_somatic_m2.filter.vcf.gz |egrep '#|PASS' | {bcftools} view -Oz -o {out}/{tumor}_{normal}_somatic_m2.filter.pass.vcf.gz
+{gatk4} IndexFeatureFile -I  {out}/{tumor}_{normal}_somatic_m2.filter.pass.vcf.gz
 """.format(gatk4=gatk4,mem=mem,ref=ref,interval=interval,out=output_dir,pon=pon,
-           input=input_dir,tumor=tumor,normal=normal,germline=germline,af=af)
+           input=input_dir,tumor=tumor,normal=normal,germline=germline,af=af,bcftools=bcftools)
         else:
             cmd_somatic = """### somatic mutation ###
 {gatk4} --java-options "-Xmx{mem}g"  Mutect2 \\
@@ -146,10 +173,13 @@ def mutect2(input_dir,output_dir,ref,tumor,normal,interval,pon=None,germline=Non
 -O {out}/{tumor}_{normal}_somatic_m2.vcf.gz
 ###filter###
 {gatk4} --java-options "-Xmx{mem}g" FilterMutectCalls \\
+-R {ref} \\
 -V {out}/{tumor}_{normal}_somatic_m2.vcf.gz \\
 -O {out}/{tumor}_{normal}_somatic_m2.filter.vcf.gz
+zcat {out}/{tumor}_{normal}_somatic_m2.filter.vcf.gz |egrep '#|PASS' | {bcftools} view -Oz -o {out}/{tumor}_{normal}_somatic_m2.filter.pass.vcf.gz
+{gatk4} IndexFeatureFile -I  {out}/{tumor}_{normal}_somatic_m2.filter.pass.vcf.gz
 """.format(gatk4=gatk4, mem=mem, ref=ref, interval=interval, out=output_dir, pon=pon,
-                       input=input_dir, tumor=tumor, normal=normal)
+                       input=input_dir, tumor=tumor, normal=normal,bcftools=bcftools)
     elif not pon and not normals_pon:
         if germline:
             cmd_somatic = """### somatic mutation ###
@@ -188,11 +218,14 @@ def mutect2(input_dir,output_dir,ref,tumor,normal,interval,pon=None,germline=Non
 
 ### filter
 {gatk4} --java-options "-Xmx{mem}g" FilterMutectCalls \\
+-R {ref} \\
 -V {out}/{tumor}_{normal}_somatic_m2.vcf.gz \\
 --contamination-table {out}/{tumor}.calculatecontamination.table \\
 -O {out}/{tumor}_{normal}_somatic_m2.filter.vcf.gz
+zcat {out}/{tumor}_{normal}_somatic_m2.filter.vcf.gz |egrep '#|PASS' | {bcftools} view -Oz -o {out}/{tumor}_{normal}_somatic_m2.filter.pass.vcf.gz
+{gatk4} IndexFeatureFile -I  {out}/{tumor}_{normal}_somatic_m2.filter.pass.vcf.gz
 """.format(gatk4=gatk4,mem=mem,ref=ref,interval=interval,out=output_dir,
-           input=input_dir,tumor=tumor,normal=normal,germline=germline,af=af)
+           input=input_dir,tumor=tumor,normal=normal,germline=germline,af=af,bcftools=bcftools)
         else:
             cmd_somatic = """### somatic mutation ###
 {gatk4} --java-options "-Xmx{mem}g"  Mutect2 \\
@@ -205,10 +238,13 @@ def mutect2(input_dir,output_dir,ref,tumor,normal,interval,pon=None,germline=Non
 -O {out}/{tumor}_{normal}_somatic_m2.vcf.gz
 ###filter###
 {gatk4} --java-options "-Xmx{mem}g" FilterMutectCalls \\
+-R {ref} \\
 -V {out}/{tumor}_{normal}_somatic_m2.vcf.gz \\
 -O {out}/{tumor}_{normal}_somatic_m2.filter.vcf.gz
+zcat {out}/{tumor}_{normal}_somatic_m2.filter.vcf.gz |egrep '#|PASS' | {bcftools} view -Oz -o {out}/{tumor}_{normal}_somatic_m2.filter.pass.vcf.gz
+{gatk4} IndexFeatureFile -I  {out}/{tumor}_{normal}_somatic_m2.filter.pass.vcf.gz
 """.format(gatk4=gatk4, mem=mem, ref=ref, interval=interval, out=output_dir,
-                       input=input_dir, tumor=tumor, normal=normal)
+                       input=input_dir, tumor=tumor, normal=normal,bcftools=bcftools)
 
     return cmd_create_pon,cmd_somatic
 
